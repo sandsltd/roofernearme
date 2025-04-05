093470077abe9f7a21e2ebcf6cba41de5a19b69f
Daniel Simmons <software@saunders-simmons.co.uk>
2025-04-05 21:41:04 +0100

    Update robots.ts with better crawl directives

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/search/', '/*?q=*'],
+      disallow: ['/admin/', '/api/'],
     },
     sitemap: 'https://www.localroofernearme.co.uk/sitemap.xml',
   };
